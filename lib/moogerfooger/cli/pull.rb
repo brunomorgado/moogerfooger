b9e1734285833f47269885886c4ff468a58a343f
Bruno Morgado <brunofcmorgado@gmail.com>
2017-08-08 22:18:06 +0200

    fix typo

--- lib/moogerfooger/cli/pull.rb
+++ lib/moogerfooger/cli/pull.rb
@@ -1,9 +1,11 @@
+require 'moog'
+
 module Mooger
   class CLI::Pull
     attr_reader :options
     def initialize(options, moog_name)
       @options = options
-      @moog = Mooger.find(moog_name)
+      @moog = Moog.find(moog_name)
     end
 
     def run
