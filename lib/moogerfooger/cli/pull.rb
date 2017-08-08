9422bf991be7e1ebc6dc57e3327198e3b9c91c34
Bruno Morgado <brunofcmorgado@gmail.com>
2017-08-08 22:23:26 +0200

    Informative msg for moog not found

--- lib/moogerfooger/cli/pull.rb
+++ lib/moogerfooger/cli/pull.rb
@@ -3,10 +3,17 @@ module Mooger
     attr_reader :options
     def initialize(options, moog_name)
       @options = options
+      @moog_name = moog_name
       @moog = Moog.find(moog_name)
     end
 
     def run
+
+      unless @moog.exists?
+        puts "Moog with name: #{@moog_name} not foung. Unable to pull."
+        return
+      end
+
       pull_remote(Mooger.default_moogs_dir.split().last + @moog.name, @moog.name, @moog.branch)
     end
 
