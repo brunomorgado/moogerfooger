db21e6b34ab2a6cc6d25224b84256d1e2ebf0a1b
Bruno Morgado <brunofcmorgado@gmail.com>
2017-08-21 17:52:28 +0200

    refactor installer to use git_helpers

---
+++ lib/moogerfooger/git_helpers.rb
@@ -0,0 +1,38 @@
+module Mooger
+  module GitHelpers
+
+    class << self 
+
+      def repo_has_changes?
+        has_staged_changes = !system("git diff --quiet --exit-code")
+        has_unstaged_changes = !system("git diff --cached --quiet --exit-code")
+        has_unstaged_changes || has_staged_changes
+      end
+
+      def remote_exists?(remote_name)
+        system "git config remote.#{remote_name}.url > /dev/null"
+      end
+
+      def add_remote(remote_name, remote_url)
+        success = system("git remote add -f #{remote_name} #{remote_url}")
+        unless success 
+          remove_remote(remote_name)
+          raise GitRemoteAddError, "Failed to add remote with name: #{remote_name} and url: #{remote_url}"
+        end
+      end
+
+      def remove_remote(remote_name)
+        return unless remote_exists?(remote_name)
+        system "git remote remove #{remote_name}"
+      end
+
+      def add_subtree(path, remote_name, branch)
+        success = system("git subtree add --prefix=#{path.to_s} #{remote_name} #{branch} --squash")
+        unless success
+          remove_remote(remote_name)
+          raise GitSubtreeAddError, "Failed to add subtree to remote with name: #{remote_name}"
+        end
+      end
+    end
+  end
+end
---
+++ lib/moogerfooger/installer/installer.rb
@@ -0,0 +1,39 @@
+require "moogerfooger/installer/git_subtree"
+
+module Mooger
+  class Installer
+    class << self
+    end
+
+    def self.install(definition, options = {})
+      installer = new(definition)
+      installer.run(options)
+      installer
+    end
+
+    def initialize(definition)
+      @definition = definition
+    end
+
+    def run(options)
+      create_moogs_dir
+      if @definition.moogs.empty?
+        #TODO: warn empty moogerfile
+        return
+      end
+      install
+    end
+
+		private
+
+		def install
+      subtree_installer = Mooger::Installer::GitSubtree.new(@definition)
+      subtree_installer.generate
+		end
+
+    def create_moogs_dir
+      return if SharedHelpers.moogs_dir.exist?
+      Dir.mkdir(SharedHelpers.moogs_dir_path)
+    end
+  end
+end
