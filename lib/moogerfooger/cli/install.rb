d8faf04eef5f0e999b3a24e9b014765a191363f9
Bruno Morgado <brunofcmorgado@gmail.com>
2017-09-02 16:28:37 +0200

    Add auto commit functionality

--- lib/moogerfooger/cli/install.rb
+++ lib/moogerfooger/cli/install.rb
@@ -9,11 +9,28 @@ module Mooger
 
       def run
         begin
-        installer = Mooger::Installer::GitSubtree.new(Mooger.definition(true), @options)
-        installer.run
+          ensure_clean
+          definition = Mooger.definition(true)
+          commit_moogerfooger_files
+          installer = Mooger::Installer::GitSubtree.new(definition, @options)
+          installer.run
         rescue => e
           Mooger.reset
         end
+      end
+
+      private
+
+      def ensure_clean
+        if GitHelpers.repo_has_changes?
+          raise GitRepoHasChangesError, "Working tree has modifications. Cannot continue"
+        end
+      end
+
+      def commit_moogerfooger_files
+        system "git add Moogerfile.lock"
+        system "git add Moogs"
+        system "git commit -m 'Update Moogerfooger files'"
       end
     end
 end
